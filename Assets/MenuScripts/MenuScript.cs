ddf9b8a26f381a9b267ab4b2a69ce411302b02c2
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2019-01-21 23:14:31 -0200

    proj com git ignore

---
+++ Assets/MenuScripts/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Animator _animator { get; set; }
+
+    public void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private bool _isMoving = false;
+    private float _speed = 2.0f;
+    private Rigidbody2D _rb2d;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    bool right = false;
+    bool left = false;
+    bool up = false;
+    bool down = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = gameObject.GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        right = Input.GetButton("GoRight");
+        left = Input.GetButton("GoLeft");
+        up = Input.GetButton("GoUp");
+        down = Input.GetButton("GoDown");
+
+        _isMoving = right || left || up || down;
+
+        if (_isMoving)
+        {
+            _animator.SetFloat("Speed", 10);
+        }
+        else
+        {
+            _animator.SetFloat("Speed", 0);
+        }
+
+        if (right)
+        {
+            transform.position += Vector3.right * _speed * Time.deltaTime;
+            _spriteRenderer.flipX = false;
+        }
+        else
+        {
+            if (left)
+            {
+                transform.position += Vector3.right * -_speed * Time.deltaTime;
+                _spriteRenderer.flipX = true;
+            }
+            else
+            {
+                if (up)
+                {
+                    transform.position += Vector3.up * _speed * Time.deltaTime;
+                }
+                else
+                {
+                    if (down)
+                    {
+                        transform.position += Vector3.up * -_speed * Time.deltaTime;
+                    }
+                    else
+                    {
+                        // not moving;
+                        _isMoving = false;
+                    }
+                }
+            }
+        }
+    }
+}
